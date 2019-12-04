a0ff1ef6875a9a87b207bd9d6be47377be468e64
YJeongKim <yjeong20s@naver.com>
2019-12-04 20:47:08 +0900

    Add 문자열 다루기 기본 to Programmers/Level 1

---
+++ Programmers/Level 1/문자열다루기기본.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    int length = s.length();
+
+    if (length != 4 && length != 6) {
+        answer = false;
+    }
+    else {
+        for (int i = 0; i < length; i++) {
+            if (isdigit(s[i]) == 0) {
+                answer = false;
+                break;
+            }
+        }
+     /* for (int i = 0; i < length; i++) {
+            if (s[i] < '0' || s[i] > '9') {
+                answer = false;
+                break;
+            }
+        } */
+    }
+
+    return answer;
+}
+
+int main(void) {
+    string s1 = "a123";
+    string s2 = "012345";
+
+    cout << "s1 : " << (solution(s1) ? "true" : "false") << endl;
+    cout << "s2 : " << (solution(s2) ? "true" : "false") << endl;
+
+    return 0;
+}
