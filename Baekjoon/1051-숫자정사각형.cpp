6a56e6dfc9f3bcfd52862a8a9ca8a2547f2cc4db
YJeongKim <yjeong20s@naver.com>
2019-12-09 19:23:46 +0900

    Add 1051 - 숫자 정사각형 to Baekjoon

---
+++ Baekjoon/1051-숫자정사각형.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int m, char square[50][50]){
+    int k = (n < m ? n : m);
+
+    while(k>0) {
+        k--;
+        for(int i=0; i+k<n; i++) {
+            for(int j=0; j+k<m; j++) {
+                if(square[i][j]==square[i+k][j] && square[i][j]==square[i][j+k] && square[i][j]==square[i+k][j+k]) {
+                    return (++k * k);
+                }
+            }
+        }
+    }
+    
+    return 1;
+}
+
+int main() {
+    int n, m;
+    char square[50][50];
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++) {
+        cin >> square[i];
+    }
+    cout << solution(n, m, square) << endl;
+
+    return 0;
+}
