8c23b71a12d783f08aba453b23dbcc1e508f90b2
YJeongKim <yjeongisme@gmail.com>
2021-03-16 18:21:37 +0900

    Add 큰 수 만들기 to Programmers/Level 2

---
+++ Programmers/Level 2/PGM42883.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/* 문제: 큰 수 만들기 / 분류: 그리디 */
+public class PGM42883 {
+    public static void main(String[] args) {
+        String[] number = { "1924", "1231234", "4177252841" };
+        int[] k = { 2, 3, 4 };
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(solution(number[i], k[i]));
+        }
+    }
+
+    public static String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        int len = number.length();
+        int maxIdx = -1;
+
+        for (int i = 0; i < len - k; i++) {
+            char maxNum = 0;
+            for (int j = maxIdx + 1; j <= k + i; j++) {
+                if (number.charAt(j) > maxNum) {
+                    maxIdx = j;
+                    maxNum = number.charAt(j);
+                }
+            }
+            answer.append(maxNum);
+        }
+        return answer.toString();
+    }
+}
--- Programmers/README.md
+++ Programmers/README.md
@@ -58,6 +58,7 @@
 | 08 | 짝지어 제거하기 | [12973](https://programmers.co.kr/learn/courses/30/lessons/12973) | 스택 | [보기](./Level%202/PGM12973.cpp) |  |
 | 09 | 124 나라의 숫자 | [12899](https://programmers.co.kr/learn/courses/30/lessons/12899) |  | [보기](./Level%202/PGM12899.cpp) |  |
 | 10 | 구명보트 | [42885](https://programmers.co.kr/learn/courses/30/lessons/42885) | 그리디 | [보기](./Level%202/PGM42885.java) |  |
+| 11 | 큰 수 만들기 | [42883](https://programmers.co.kr/learn/courses/30/lessons/42883) | 그리디 | [보기](./Level%202/PGM42883.java) |  |
 
 ### 🤟 Level 3
 
