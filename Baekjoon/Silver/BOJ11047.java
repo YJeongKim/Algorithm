5f844472a8daa3bf64a9e218b7aa305dcb867c22
YJeongKim <yjeongisme@gmail.com>
2021-04-23 21:53:17 +0900

    Add 동전 0 to Baekjoon/Silver

---
+++ Baekjoon/Silver/BOJ11047.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/* 문제: 동전 0 / 분류: 그리디 */
+public class BOJ11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        int result = findMinNumCoin(N, K, A);
+        System.out.println(result);
+        sc.close();
+    }
+
+    public static int findMinNumCoin(int n, int k, int[] a) {
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (k == 0) break;
+            if (k / a[i] > 0) {
+                count += k / a[i];
+                k %= a[i];
+            }
+        }
+        return count;
+    }
+}
